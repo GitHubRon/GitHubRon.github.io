700743a2daf12d9d0e2fd790ab22c7653e512779
Ron Eicher <ronaldmeicherii@gmail.com>
2017-03-09 18:32:23 -0500

    Minor updates for comments, validator results and icons

--- javascript/main.js
+++ javascript/main.js
@@ -1,134 +1,116 @@
-/**
- * Created by RonaldMEicherII on 1/14/17.
- */
-
 "use strict";
+/*****************************************************************************
+** Created by Ronald M. Eicher II on 1/14/17 **
+*****************************************************************************/
 $.noConflict();
 jQuery(document).ready(function ($) {
 
-// START Slide logo code //////////////////////////////////////////////////////////
+// START Slide logo code, navbar, and link scrolling code
     var logoStripe = $("div.logobar");
-    var ron = $("#myName");
-//    console.log(logoStripe);
-    var otherContent = $("nav").siblings().addBack(); //addBack() is a replacement for deprecated andSelf();
+    var otherContent = $("nav").siblings().addBack();
+    //addBack() is a replacement for deprecated andSelf()
     otherContent.hide();
-    $('#footercont').css("visibility", "hidden");
+    $("#footercont").css("visibility", "hidden");
 
     logoStripe.each(function (index) {
-            $(this)
-                .delay(200 + index * 500)
-                .show(500);
-        }
-    );
+        $(this)
+            .delay(200 + index * 500)
+            .show(500);
+    });
 
     logoStripe.eq(2).promise().then(function () {
         otherContent.fadeIn("slow");
-        //$("#slider").after($("<div>", {
-            //"id": "label",
-            //"html": "<p style='color: white'>Clock Brightness</p>"
+    });
+
+    otherContent.promise().then(function () {
+        if (screen.width > 100 && window.innerWidth > 100 && $(window).width() > 100) {
+        // The code below gives a slight initial scroll, future movements are counted as a scroll.
+        // Links do not function properly without this, except after a manual scroll.
+            window.scrollTo(0, 1);
+        // START Navbar link code to scroll on desktop (compensate for toolbars and fixed nav)
+        // Offsets are zero before the fade in, this must be done here
+        // Window height and screen height are different due to Mac tool bar
+        // $(window).height() needs used for responsive design mode, screen.height remains desktop height.
+            var windowHeight = $(window).height();
+            var screenHeight = screen.height;
+            var diff = screenHeight - windowHeight;
+            var navHeight = $("#addedNav").outerHeight();
+            // The code below is for ipads
+            if (diff < 50) {
+                navHeight = $("#addedNav").outerHeight(true);
+            }
+            // Find all items with href beginning with '#link'
+            var navItems = $("[href^='#link']");
+            // Loop the navbar links, set scroll amount to link target offset compensating
+            // for the Mac header and the navbar height of 50.
+            navItems.each(function () {
+                var targetId = $(this).attr("href");
+                var item = $(targetId);
+                var scrollAmount = ((item.offset().top) - (screenHeight - windowHeight + navHeight));
+                $(this).on("click", function (event) {
+                    event.preventDefault();
+                    window.scrollTo(0, scrollAmount);
+                });
+            });
+        }
+    });
+    // The code below activates the menu icon click on smaller devices when any link is clicked.
+    // Without the code below the uncollapsed menu blocks the screen, forces user to click again, and skews the scroll amount!
+    if ($(window).width() < 1100) {
+        $("[href^='#link']").on("click", function () {
+            $("button.navbar-toggle").trigger("click");
         });
+    }
 
-        //$("#label")
-           // .append("<p style='color: white'>50%</p>")
-           // .css("margin", ".5vh 0 0 0");
+// End Slide logo code, navbar, and link scrolling code
 
+// START Navbar id="addedDiv" code to "stick" to the top
+    $(window).on("scroll", function () {
+        // Fix navbar for all devices, leaving conditionals as a reminder of option not to fix navbar for smaller devices.
+        var bannerHeight = $(".logobar").height() * 3;
+        if ($(window).scrollTop() > bannerHeight && $(window).width() > 100 && screen.width > 100) {
+            $("#addedNav").addClass("navbar-fixed-top");
+        } else if ($(window).scrollTop() < bannerHeight) {
+            $("#addedNav").removeClass("navbar-fixed-top");
+        }
+    });
+// END Navbar id="addedDiv" code to "stick" to the top
 
-        otherContent.promise().then(function () {
-			if (screen.width > 100 && window.innerWidth > 100 && $(window).width() > 100) {
-				window.scrollTo(0,1);
-			// START Navbar link code to scroll on desktop (compensate for toolbars and fixed nav)
-			// Offsets are zero before the fade in //////
-			// Window height and screen height are different due to Mac tool bar
-				var windowHeight = $(window).height(); 
-				var screenHeight = screen.height; 
-				var diff = screenHeight - windowHeight; 
-				var navHeight = $('#addedNav').outerHeight();
-				// Find all items with href beginning with '#link'
-				// The code below if for ipads
-				if (diff < 50) {
-					navHeight = $('#addedNav').outerHeight(true); 
-				}
-				var navItems = $('[href^="#link"]');
-				// Loop the navbar links, set scroll amount to link target offset compensating
-				// for the Mac header and the navbar height of 50. 
-				navItems.each(function () {
-					var targetId = $(this).attr("href");
-					var item = $(targetId); 
-					var scrollAmount =((item.offset().top) - (screenHeight - windowHeight + navHeight));
-					$(this).on("click", function (event) {
-						event.preventDefault();
-						window.scrollTo(0,scrollAmount); 
-					// scrollspy makes obsolete	$(this).parent().addClass("active");
-					});
-						
-				});
-			}
-		});
-		// The code below activates the menu icon click on smaller devices when any link is clicked. 
-		// Without the code below the uncollapsed menu blocks the screen, forces user to click again, and skews the scroll amount!
-		if ($(window).width() < 1100) {
-			$('[href^="#link"]').on("click", function(){
-				$('button.navbar-toggle').trigger("click");
-			});
-		}
-
-			// END Navbar code /////////////////////////////////////////////////////////
-
-            //holdFooter();
-            //$("#videoholder").load("videomi.html");
-        
-
-
-
-// End Slide logo code /////////////////////////////////////////////////////////////////////////////////////////////////
-// START Navbar id="addedDiv" code to "stick" to the top //////////////////////
-$(window).on("scroll", function () {
-	// Fix navbar for all devices, leaving conditionals as a reminder of option not to fix navbar for smaller devices.
-	var windowHeight = $(window).height(); 
-	var screenHeight = screen.height; 
-	var diff = screenHeight - windowHeight; 
-	var bannerHeight = $('.logobar').height() * 3; 
-	
-	if (($(window).scrollTop() > bannerHeight && $(window).width() > 100 && screen.width > 100)) {
-		$("#addedNav").addClass("navbar-fixed-top");
-	}
-	else if ($(window).scrollTop() < bannerHeight) { 
-	$("#addedNav").removeClass("navbar-fixed-top");
-	}; 
-});
-
-    // START Opacity slider code //////////////////////////////////////////////////////////
+// START Opacity slider code
     $("#slider").slider({
         value: 50,
         min: 0
     });
 
-    $("#clockDisplay").css("opacity", .50);
+    $("#clockDisplay").css("opacity", 0.50);
 
 
 
-    $("#slider").on("slide", function (event, ui) {
+    $("#slider").on("slide", function (ui) {
         var valueMessage = ui.value;
         $("#adjValue").text(valueMessage);
-        var setOpacity = ui.value/100;
-        $('#clockDisplay').css("opacity", setOpacity);
+        var setOpacity = ui.value / 100;
+        $("#clockDisplay").css("opacity", setOpacity);
     });
-// END Opacity slider code //////////////////////////////////////////////////////////
-// START Clock code ////////////////////////////////////////////////////////////////////////////////////////////////////
+// END Opacity slider code
 
+// START Clock code
     function timeDisplay() {
+        var text;
         var currTime = new Date();
         var hourVal = currTime.getHours();
         var minVal = currTime.getMinutes();
-        (minVal < 10) ? minVal = "0" + minVal : minVal = minVal;
+        //(minVal < 10) ? minVal = "0" + minVal : minVal = minVal;
+        minVal = (minVal < 10) ? "0" + minVal : minVal;
         var secVal = currTime.getSeconds();
-        (secVal < 10) ? secVal = "0" + secVal : secVal = secVal;
+        //(secVal < 10) ? secVal = "0" + secVal : secVal = secVal;
+        secVal = (secVal < 10) ? "0" + secVal : secVal;
         if (hourVal >= 0 && hourVal < 12) {
-            var text = "GOOD MORNING";
+            text = "GOOD MORNING";
         } else if (hourVal >= 12 && hourVal < 17) {
-            var text = "GOOD AFTERNOON";
+            text = "GOOD AFTERNOON";
         } else {
-            var text = "GOOD EVENING";
+            text = "GOOD EVENING";
         }
         return hourVal + ":" + minVal + ":" + secVal + "   <span id='greet'>" + text + "</span>";
     }
@@ -137,19 +119,33 @@ $(window).on("scroll", function () {
         $("#clockDisplay").html(timeDisplay());
     }
 
-    var runClock = setInterval(startClock, 500);
+    setInterval(startClock, 500);
+// END Clock code
 
+// START Load video code 
+    $("#loadButton").on("click", function () {
+        $("#myVideo").load("videomi.html");
+    });
+// END Load video code
 
-// END Clock code //////////////////////////////////////////////////////////////////////////////////////////////////////
-/////// START LOAD CODE //////////////////////
-    $('#loadButton').on("click", function () {
-		$('#myVideo').load("videomi.html");
-	});
-// START print resume button code ////////////////////////////////////////////
-    $('#printResume').on("click", function () {
-		window.print();
-	});
-// END print resume button code /////////////////////////////////////////////
-});
+// START print resume button code
+    $("#printResume").on("click", function () {
+        window.print();
+    });
+// END print resume button code
 
+// START link button code
+    $("#lugnuts").click(function () {
+        window.open("http://www.webweavin.com");
+    });
+    $("#geocache").click(function () {
+        window.open("http://www.geocaching.com");
+    });
+    $("#chess").click(function () {
+        window.open("http://www.chess.com");
+    });
+// END link button code
+
+}); // END jQuery ready block
+   
 
